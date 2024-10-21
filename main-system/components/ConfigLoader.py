d914f172dd128dc8d83d3356eecf1f3e3145c9c7
CamH123 <Cameron.h.1732@gmail.com>
2024-10-20 20:41:54 -0500

    made strucutre for configloader

---
+++ main-system/components/ConfigLoader.py
@@ -0,0 +1,21 @@
+import json
+
+class ConfigLoader:
+    def __init__(self, config_filepath: str):
+        self.config_filepath = config_filepath
+        self.loaded_configs = None
+
+    def laod_configs():
+        """
+        Reads in and loads all of the configurations from configs.json
+        """
+        pass
+    
+    def fetch_imaging_configs():
+        pass
+
+    def fetch_exstate_configs():
+        pass
+
+    def fetch_all_configs():
+        pass
