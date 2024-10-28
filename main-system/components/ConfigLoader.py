9348a1c1c0053cdfde28b9db57270b9cae425eac
CamH123 <Cameron.h.1732@gmail.com>
2024-10-27 20:30:26 -0500

    completed 1st rendition of conigloader and json file (put random data inside)

--- main-system/components/ConfigLoader.py
+++ main-system/components/ConfigLoader.py
@@ -3,19 +3,27 @@ import json
 class ConfigLoader:
     def __init__(self, config_filepath: str):
         self.config_filepath = config_filepath
-        self.loaded_configs = None
-
-    def laod_configs():
-        """
-        Reads in and loads all of the configurations from configs.json
-        """
-        pass
+        self.loaded_configs = json.load(open(config_filepath))
     
-    def fetch_imaging_configs():
-        pass
+    def fetch_imaging_configs(self):
+        """
+        returns all imaging configs
+        """
+        return self.loaded_configs["imaging"]
 
-    def fetch_exstate_configs():
-        pass
+    def fetch_exstate_configs(self):
+        """
+        returns all exteral configs
+        """
+        return self.loaded_configs["exstate"]
 
-    def fetch_all_configs():
-        pass
+    def fetch_all_configs(self):
+        """
+        returns all configs
+        """
+        return self.loaded_configs
+
+config = ConfigLoader("main-system\components\config.json")
+print(config.fetch_all_configs())
+print(config.fetch_exstate_configs())
+print(config.fetch_imaging_configs())
