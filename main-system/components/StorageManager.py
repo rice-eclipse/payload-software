ee791c012af391cb3ab607494e8501648e1c036b
calvin <calvintwong25@gmail.com>
2024-11-10 21:23:44 -0600

    objectified the storage manager

--- main-system/components/StorageManager.py
+++ main-system/components/StorageManager.py
@@ -1,61 +1,80 @@
 import shutil
 import os
+class StorageManager:
+    def __init__(self, path, angle_threshold, altitude_threshold):
+        """
+        Initialize the StorageManager object.
 
-def clean_images(path):
+        Parameters
+        ----------
+        path : str
+            The path to the directory containing the images to be cleaned.
+        angle_threshold : tuple of two floats
+            The minimum and maximum angle thresholds to filter images by.
+        altitude_threshold : tuple of two floats
+            The minimum and maximum altitude thresholds to filter images by.
+        """
+        self.path = path
+        self.angle_min_threshold = angle_threshold[0]
+        self.angle_max_threshold = angle_threshold[1]
+        self.altitude_min_threshold = altitude_threshold[0]
+        self.altitude_max_threshold = altitude_threshold[1]
+        
+    def clean_images(self):
 
-    """
-    Clean images in the given path by deleting images with altitudes above/below a certain threshold, 
-    or angles above/below a certain threshold. 
+        """
+        Clean images in the given path by deleting images with altitudes above/below a certain threshold, 
+        or angles above/below a certain threshold. 
 
-    Parameters
-    ----------
-    path : str
-        The path to the directory containing the images to be cleaned.
+        Parameters
+        ----------
+        path : str
+            The path to the directory containing the images to be cleaned.
 
-    Returns
-    -------
-    None
-    """
-    # iterate through all images in the path
-    for file in os.listdir(path):
-        if file.endswith(".jpg"):
-            try:
-                # files are being stored as f"{altitude}&{angle}&{time}.jpg"
-                altitude, angle, timestamp = file.split("&")
-                altitude = float(altitude)
-                angle = float(angle)
-                
-                # remove the images that are above/below a certain threshold in altitude or angle
-                if altitude > 2000 or altitude < 20:
-                    os.remove(os.path.join(path, file))
-                elif angle > 45 or angle < -45:
-                    os.remove(os.path.join(path, file))
-    
-            except ValueError:
-                print(f"Skipping file with invalid format: {file}")
+        Returns
+        -------
+        None
+        """
+        # iterate through all images in the path
+        for file in os.listdir(self.path):
+            if file.endswith(".jpg"):
+                try:
+                    # files are being stored as f"{altitude}&{angle}&{time}.jpg"
+                    altitude, angle, timestamp = file.split("&")
+                    altitude = float(altitude)
+                    angle = float(angle)
+                    
+                    # remove the images that are above/below a certain threshold in altitude or angle
+                    if altitude > self.altitude_max_threshold or altitude < self.altitude_min_threshold:
+                        os.remove(os.path.join(self.path, file))
+                    elif angle > self.angle_max_threshold or angle < self.angle_min_threshold:
+                        os.remove(os.path.join(self.path, file))
+        
+                except ValueError:
+                    print(f"Skipping file with invalid format: {file}")
 
-def clean_storage(path):
-    """
-    Check the storage usage of the given path. If the storage usage is above 80%, clean the images in the path by calling clean_images. Return True if the storage has been cleaned, False otherwise.
+    def clean_storage(self):
+        """
+        Check the storage usage of the given path. If the storage usage is above 80%, clean the images in the path by calling clean_images. Return True if the storage has been cleaned, False otherwise.
 
-    Parameters
-    ----------
-    path : str
-        The path to the directory containing the images to be cleaned.
+        Parameters
+        ----------
+        path : str
+            The path to the directory containing the images to be cleaned.
 
-    Returns
-    -------
-    bool
-    """
-    # get current storage usage
-    total, used, free = shutil.disk_usage(path)
-    # get percentage of storage used
-    usage_percent = (used / total) * 100
-    # if storage usage is above 80%, clean the images
-    if usage_percent > 80:
-        clean_images(path)
-        return True
-    else:
-        return False  
+        Returns 
+        -------
+        True if cleaning happened, False else
+        """
+        # get current storage usage
+        total, used, free = shutil.disk_usage(self.path)
+        # get percentage of storage used
+        usage_percent = (used / total) * 100
+        # if storage usage is above 80%, clean the images
+        if usage_percent > 80:
+            self.clean_images()
+            return True
+        else:
+            return False  
     
 
