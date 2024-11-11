2082ade331bf0d8a46694ebe37a35f440c8564f4
tyytyyy <tom3yuan@gmail.com>
2024-11-10 20:47:37 -0600

    started AccelReader and GyroscopeReader class

--- main-system/components/GyroscopeReader.py
+++ main-system/components/GyroscopeReader.py
@@ -1,28 +1,31 @@
+import board
+import adafruit_lsm9ds1
+import math
+
 class GyroscopeReader:
 
-    def __init__(self):
+    def __init__(self, timeclock):
+        #initalizes the gyroscope-reader class
+        #Uses the i2c pins. Edit below to change
+        i2c = board.I2C()
+        self.sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
+        self.timer = timeclock
         self.past_velocity = []
-        self.time = 0 #time between readings
         self.angle = 0
         #start device here
 
-    def get_curr_velocity():
-        #get x velocity
-        #get y velocity
-        #get z velocity
-        curr_angle_x = "" #get current velocity here
-        curr_angle_y = "" #get current velocity here
-        curr_angle_z = "" #get current velocity here
-        return (curr_angle_x, curr_angle_y, curr_angle_z)
+    def get_curr_angle(self):
+        #get current angle in degrees units
+        ax, ay, az = self.sensor.gyro
+        tilt_angle_rad = math.acos(az / math.sqrt(ax**2 + ay**2 + az**2))
+        tilt_angle_deg = math.degrees(tilt_angle_rad)
+        self.store_angle(tilt_angle_deg)
+        return(tilt_angle_deg)
     
     def get_last_angle(self):
+        #get the last recorded angle
         return self.pastAngles[-1]
     
     def store_angle(self, angle):
+        #stores the last read angle
         self.pastAngles.append(angle)
-
-    def get_curr_angle(self):
-        velocity = self.get_curr_velocity()
-        #integrate delta angle over total t (integrate past_velocity, multiply each value with self.time)
-        #or add current velocity multiplied by current velocity (keep current angle instead of a list. Depend on if we need past velocity for anything)
-        pass
