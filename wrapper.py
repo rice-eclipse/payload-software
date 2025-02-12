b9543dab5c4d0c54e6ed6c0ea181e6db7434d77e
shinsean <seunggyu.shin1@gmail.com>
2025-02-11 19:05:06 -0600

    Add simulated data readers into wrapper

--- wrapper.py
+++ wrapper.py
@@ -3,6 +3,10 @@ from main_system.components import AltimeterReader
 from main_system.components import GyroscopeReader
 from main_system.components import AccelReader
 
+from test_system.components import SimAltReader
+from test_system.components import SimGyroReader
+from test_system.components import SimAccelReader
+
 maincontroller = BigWrapper(AltimeterReader, GyroscopeReader, AccelReader)
 
 maincontroller.run()
