cc132128ebf289da9c9d178cff931323527473aa
shinsean <seunggyu.shin1@gmail.com>
2025-03-07 19:45:57 -0600

    Split the MSS exec into two wrappers
    wrapper_live.py is for the real sensor stream while wrapper_sim.py is
    for the testing environment.

---
+++ wrapper_sim.py
@@ -0,0 +1,33 @@
+import signal
+import sys
+
+from main_system.components.BigWrapper import BigWrapper
+
+from test_system.components.SimAltReader import SimAltReader
+from test_system.components.SimGyroReader import SimGyroReader
+from test_system.components.SimAccelReader import SimAccelReader
+
+# An asynchronous signal handler.
+# If the user hits CTRL+C, dump the logs to disk and exit regardless of our position in the BigWrapper loops.
+def handle_sigint(signum, frame):
+    print('Handling SIGINT. Dumping logs.')
+    
+    if (maincontroller != None):
+        maincontroller.force_write_logs()
+        sys.exit(0)
+
+# All configs related to running the MainSoftwareSystem in its real launch configuration
+# is contained in config.json.
+
+# Configurations and pathing for running the MainSoftwareSystem in the testing environment 
+# are still primarily contained in config.json, but the paths for the data sources for the 
+# simulated readers need to be configured in their respective files.
+
+# Note that the simulated readers need to have their paths reconfigured in their files to the desired data sources.
+maincontroller = BigWrapper(SimAltReader, SimGyroReader, SimAccelReader)
+
+# Install the signal handler.
+signal.signal(signal.SIGINT, handle_sigint)
+
+# Run the main loop.
+maincontroller.run()
